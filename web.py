eb1f6117bc326e23ceec363d982d9b8a9c4d0016
hirogwa <hirogwa@gmail.com>
2015-06-07 01:46:40 -0700

    add user

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
-from flask import Flask, Response, render_template, request, redirect
+from flask import Flask, Response, render_template, request, redirect, url_for
 from bs4 import BeautifulSoup
+import flask_login
 import json
 import os
 import urllib.error
@@ -12,55 +13,126 @@ import settings
 import s3_store
 
 app = Flask(__name__)
+app.secret_key = settings.SECRET_KEY
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.get_by_id(user_id)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'POST' == request.method:
+        args = request.form
+        username = args.get('username')
+        password = args.get('password')
+        user = models.User.get_by_credentials(username, password)
+        if user:
+            flask_login.login_user(user)
+        return redirect(url_for('login'))
+
+    if 'GET' == request.method:
+        if flask_login.current_user.is_authenticated():
+            return redirect(url_for('dashboard'))
+        return render_template('login.html')
+
+
+@app.route('/logout', methods=['GET'])
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard', methods=['GET'])
+@flask_login.login_required
+def dashboard():
+    kwargs = dashboard_template_args(sidebar_overview='active')
+    return render_template('dashboard-overview.html', **kwargs)
 
 
 @app.route('/show/new', methods=['GET'])
-def create_show():
-    if 'GET' == request.method:
-        return render_template('show.html')
+@flask_login.login_required
+def page_create_show():
+    return page_show()
 
 
 @app.route('/show/<show_id>', methods=['GET'])
-def load_show(show_id):
-    if 'GET' == request.method:
-        return render_template('show.html', show_id=show_id)
+@flask_login.login_required
+def page_load_show(show_id):
+    return page_show(show_id)
+
+
+def page_show(show_id=None):
+    kwargs = dashboard_template_args(sidebar_show='active')
+    if show_id:
+        kwargs['show_id'] = show_id
+    return render_template('dashboard-show.html', **kwargs)
 
 
 @app.route('/show', methods=['GET', 'POST'])
+@flask_login.login_required
 def show():
     if 'GET' == request.method:
         show_id = request.args['show_id']
-        # TODO
-        user = models.User.create_test_user()
+        user = flask_login.current_user
         show = models.Show.get_by_id(show_id, user)
         return json_response(show.export())
 
     if 'POST' == request.method:
-        user = models.User.create_test_user()
+        user = flask_login.current_user
         show = models.Show.create_new(user, **request.get_json())
         show.save()
         return json_response(show.export())
 
 
 @app.route('/episode/new', methods=['GET'])
-def create_episode():
-    if 'GET' == request.method:
-        return render_template('episode.html')
+@flask_login.login_required
+def page_create_episode():
+    return page_episode()
 
 
 @app.route('/episode/<episode_id>', methods=['GET'])
-def load_episode(episode_id):
-    if 'GET' == request.method:
-        return render_template('episode.html', episode_id=episode_id)
+@flask_login.login_required
+def page_load_episode(episode_id):
+    return page_episode(episode_id)
+
+
+def page_episode(episode_id=None):
+    kwargs = dashboard_template_args(sidebar_episodes='active')
+    if episode_id:
+        kwargs['episode_id'] = episode_id
+    return render_template('dashboard-episode.html', **kwargs)
 
 
 @app.route('/episode-list', methods=['GET'])
+@flask_login.login_required
 def episode_list():
-    if 'GET' == request.method:
-        return render_template('episode-list.html')
+    kwargs = dashboard_template_args(sidebar_episodes='active')
+    return render_template('dashboard-episode-list.html', **kwargs)
+
+
+def dashboard_template_args(**kwargs):
+    user = flask_login.current_user
+    result = {
+        'show_id': user.shows[0] if len(user.shows) else 'new'
+    }
+    for k, v in kwargs.items():
+        result[k] = v
+    return result
 
 
 @app.route('/episode', methods=['GET', 'POST'])
+@flask_login.login_required
 def episode():
     if 'POST' == request.method:
         # TODO
@@ -72,14 +144,22 @@ def episode():
         return json_response(ep.export())
 
 
+@app.route('/people', methods=['GET'])
+def page_people():
+    kwargs = dashboard_template_args(sidebar_people='active')
+    return render_template('dashboard-people.html', **kwargs)
+
+
 @app.route('/episodes', methods=['GET'])
+@flask_login.login_required
 def episodes():
     if 'GET' == request.method:
         return json_response(list(
             map(lambda x: x.export(), models.Episode.get_list())))
 
 
-@app.route('/media', methods=['POST'])
+@app.route('/media', methods=['POST', 'GET'])
+@flask_login.login_required
 def media():
     if 'POST' == request.method:
         uploaded_file = request.files['file']
@@ -93,15 +173,28 @@ def media():
             'result': 'success'
         })
 
+    if 'GET' == request.method:
+        kwargs = dashboard_template_args(sidebar_media='active')
+        return render_template('dashboard-media.html', **kwargs)
+
 
 @app.route('/media/<media_id>', methods=['GET'])
+@flask_login.login_required
 def retrieve_media(media_id):
     return redirect(
         urllib.parse.urljoin(
             settings.S3_HOST, '%s/%s' % (settings.S3_BUCKET_NAME, media_id)))
 
 
+@app.route('/media-list', methods=['GET'])
+@flask_login.login_required
+def retrieve_media_list():
+    media_list = models.Media.get_list(flask_login.current_user)
+    return json_response(map(lambda x: x.export(), media_list))
+
+
 @app.route('/link-info', methods=['GET'])
+@flask_login.login_required
 def link_info():
     try:
         req = urllib.request.Request(request.args['url'], method='GET')
@@ -135,6 +228,7 @@ def link_info():
 
 
 @app.route('/personality', methods=['GET', 'POST'])
+@flask_login.login_required
 def personality():
     if 'GET' == request.method:
         pass
@@ -144,6 +238,7 @@ def personality():
 
 
 @app.route('/twitter-user', methods=['GET'])
+@flask_login.login_required
 def twitter_user():
     user = externals.twitter_user(request.args['screen_name'])
     return json_response({
@@ -155,6 +250,7 @@ def twitter_user():
 
 
 @app.route('/twitter-user-search', methods=['GET'])
+@flask_login.login_required
 def twitter_user_search():
     users = externals.twitter_user_search(request.args['q'])
     return json_response(
@@ -181,8 +277,10 @@ def temp_filepath(filename):
     return os.path.join(settings.TEMP_FILES_DIR, filename)
 
 
-def json_response(dict_data):
-    return Response(json.dumps(dict_data), mimetype='application/json')
+def json_response(data):
+    if type(data) is map:
+        data = list(data)
+    return Response(json.dumps(data), mimetype='application/json')
 
 
 if __name__ == '__main__':
