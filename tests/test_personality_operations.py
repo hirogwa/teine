c05f0d0a6413a9677db86e684ca3cb14d8ebc817
hirogwa <hirogwa@gmail.com>
2016-01-28 21:45:25 +0900

    list personality operation tests

---
+++ tests/test_personality_operations.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+class TestPersonalityOperation(unittest.TestCase):
+    def test_get_or_create_non_twitter(self):
+        self.assertTrue(False)
+
+    def test_get_or_create_existing_personality(self):
+        self.assertTrue(False)
+
+    def test_get_or_create_new_personality(self):
+        self.assertTrue(False)
+
+    def test_add_host_to_episode_host_not_found(self):
+        self.assertTrue(False)
+
+    def test_add_host_to_episode(self):
+        self.assertTrue(False)
+
+    def test_remove_host_from_episode_host_not_found(self):
+        self.assertTrue(False)
+
+    def test_remove_host_from_episode(self):
+        self.assertTrue(False)
+
+    def test_add_guest_to_episode_guest_not_found(self):
+        self.assertTrue(False)
+
+    def test_add_guest_to_episode(self):
+        self.assertTrue(False)
+
+    def test_remove_guest_from_episode_guest_not_found(self):
+        self.assertTrue(False)
+
+    def test_remove_guest_from_episode(self):
+        self.assertTrue(False)
+
+    def test_delete_target_not_found(self):
+        self.assertTrue(False)
+
+    def test_delete(self):
+        self.assertTrue(False)
+
+    def test_people_to_ids(self):
+        self.assertTrue(False)
